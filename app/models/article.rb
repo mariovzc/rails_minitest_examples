80fcfcaeb0ce48a36d5c4075aecc7adef8344172
¨Mario <mario.vizcainos@gmail.com>
2017-11-13 20:35:50 -0500

    created unit test

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+
+  def capitalize_title
+    self.title = self.title.capitalize!
+  end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "article should have a title" do
+    @article = Article.new
+    assert_not @article.save
+  end
+  
+  test "capitalize_title should return a Article with capitalized title" do
+    @article = Article.new(title: 'hola')
+    @article.capitalize_title
+
+    assert_equal 'Hola', @article.title
+  end
+
 end
